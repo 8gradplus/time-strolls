9818492176e0d2f5e6752adff3e6a03403cd864f
Viktor Atalla <v.atalla@debtray.de>
2025-05-05 12:52:55 +0200

    use titiler for local development

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,52 +1,27 @@
-from fastapi import FastAPI, Depends
-from fastapi.responses import StreamingResponse
-from rio_tiler.io import Reader
-import io
-from rio_tiler.profiles import img_profiles
-from fastapi.responses import Response
-from rio_tiler.utils import render
-from PIL import Image
-from helpers.fs import serve_static_binary
+from fastapi import FastAPI
+from titiler.core.factory import TilerFactory
+
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/timetiles/preview")
-def get_preview():
-    with Reader(TIF) as reader:
-            img = reader.read()
-    content = img.render(img_format="PNG", **img_profiles.get("png"))
-    return Response(content, media_type="image/png")
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins (for development - be more specific in production)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/timetiles/{t}/{z}/{x}/{y}")
-def get_tile(z: int, x: int, y: int):
-    with Reader(TIF) as reader:
-            img = reader.tile(x, y, z)
-    content = img.render(img_format="PNG", **img_profiles.get("png"))
-    return Response(content, media_type="image/png")
+# Create a TilerFactory for Cloud-Optimized GeoTIFFs
+cog = TilerFactory()
 
-
-    # I am fed up with titiler!
-    """
-    cog = TilerFactory()
+# Register all the COG endpoints automatically
+app.include_router(cog.router, tags=["Cloud Optimized GeoTIFF"])
 
 
-    # Add CORS middleware
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],  # Allows all origins (for development - be more specific in production)
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-    # Create a TilerFactory for Cloud-Optimized GeoTIFFs
-
-    # Register all the COG endpoints automatically
-    app.include_router(cog.router, tags=["Cloud Optimized GeoTIFF"])
-
-
-    # Optional: Add a welcome message for the root endpoint
-    @app.get("/")
-    def read_index():
-        return {"message": "Welcome to Timestrolls"}
-    """
+# Optional: Add a welcome message for the root endpoint
+@app.get("/")
+def read_index():
+    return {"message": "Welcome to TiTiler"}
