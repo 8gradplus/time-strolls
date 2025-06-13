f4a445387a95c0e0b6a1cade76bc7da15fcb9e1e
Viktor Atalla <v.atalla@debtray.de>
2025-06-14 01:02:05 +0200

    added caroussel - wip

--- frontend/src/LocationInfo/Images.jsx
+++ frontend/src/LocationInfo/Images.jsx
@@ -1,12 +1,47 @@
+import { useState } from "react";
 import { Box } from "@mui/material";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
 
 const DisplayImages = (props) => {
   const { images } = props;
+
+  // Image carroussel
+  const [lightboxOpen, setLightboxOpen] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
   if (!images || images.length === 0) return null;
+  const slides = images.map((img) => ({ src: img.url }));
+
+  const openLightbox = (index) => {
+    setCurrentIndex(index);
+    setLightboxOpen(true);
+    // Enter fullscreen mode automatically
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen();
+    }
+  };
+
+  const closeLightbox = () => {
+    setLightboxOpen(false);
+    // Exit fullscreen mode if active
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else if (document.webkitFullscreenElement) {
+      document.webkitExitFullscreen();
+    }
+  };
+
   return (
     <>
       {images.map((image, idx) => (
-        <Box key={`img-${idx}-${image.url}`} sx={{ mb: 1 }}>
+        <Box
+          key={`img-${idx}-${image.url}`}
+          sx={{ mb: 1 }}
+          onClick={() => openLightbox(idx)}
+        >
           <img
             src={image.url}
             alt={`img-${idx}-${image.url}`}
@@ -15,6 +50,16 @@ const DisplayImages = (props) => {
           />
         </Box>
       ))}
+      {/* Image Caroussel */}
+      {lightboxOpen && (
+        <Lightbox
+          open={lightboxOpen}
+          close={closeLightbox}
+          index={currentIndex}
+          slides={slides}
+          animation={{ fade: 450, swipe: 350 }}
+        />
+      )}
     </>
   );
 };
