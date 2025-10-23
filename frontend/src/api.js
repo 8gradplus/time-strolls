ddad9dd8e90cc49543ee6488a6b4ca033150b452
Viktor Atalla <v.atalla@debtray.de>
2025-10-24 00:36:35 +0200

    adapted tour route in frontent

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 export const api = {
   locationInfo: (id) => `/api/locations/${id}`,
   locations: "/api/locations/",
-  tour: (id) => `/api/tracks/${id}`,
-  tours: "/api/tracks/",
+  tour: (id) => `/api/tours/${id}`,
+  tours: "/api/tours/",
 };
