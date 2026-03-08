383bee1609d4e568565f4192c6701264e3332c6f
Viktor Atalla <v.atalla@debtray.de>
2026-03-08 15:21:02 +0100

    marker clustering

--- frontend/src/LocationMarkers/LocationMarkers.jsx
+++ frontend/src/LocationMarkers/LocationMarkers.jsx
@@ -1,21 +1,28 @@
+import MarkerClusterGroup from "react-leaflet-cluster";
 import LocationMarker from "./LocationMarker";
 
 const LocationMarkers = (props) => {
   const { open, onClick, locations } = props;
   if (!open) return null;
+
   return (
-    <>
+    <MarkerClusterGroup
+      chunkedLoading
+      maxClusterRadius={60}
+      spiderfyOnMaxZoom={true}
+      showCoverageOnHover={false}
+      zoomToBoundsOnClick={true}
+    >
       {locations.map((location, idx) => (
         <LocationMarker
           key={location.id || idx}
           lat={location.lat}
           lon={location.lon}
           label={location.name}
-          // Todo: onClick(location.id) upon API beause this component would call the conent by itself
           onClick={onClick(location.id)}
         />
       ))}
-    </>
+    </MarkerClusterGroup>
   );
 };
 
